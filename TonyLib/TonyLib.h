cec7f21778e72d9a5f4482ee4a90aba0952e6a90
Numpol Poldongnok <numpol.pol@ascendcorp.com>
2020-11-07 16:58:43 +0700

    "Initial project setup"

---
+++ TonyLib/TonyLib.h
@@ -0,0 +1,19 @@
+//
+//  TonyLib.h
+//  TonyLib
+//
+//  Created by Numpol Poldongnok on 7/11/2563 BE.
+//  Copyright © 2563 Numpol Poldongnok. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TonyLib.
+FOUNDATION_EXPORT double TonyLibVersionNumber;
+
+//! Project version string for TonyLib.
+FOUNDATION_EXPORT const unsigned char TonyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TonyLib/PublicHeader.h>
+
+
---
+++ TonyLibExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TonyLibExamples
+//
+//  Created by Numpol Poldongnok on 7/11/2563 BE.
+//  Copyright © 2563 Numpol Poldongnok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
